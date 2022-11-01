4718bbb8b725039d73bec52b772fc9442cad7b27
themikep82 <themikep@gmail.com>
2022-10-31 22:30:20 -0700

    formatting

--- python/credentials.py
+++ python/credentials.py
@@ -1,6 +1,6 @@
 credentials ={
-    'user'      :	'foo',
-    'database'  :	'bar',
+    'user'      :   'foo',
+    'database'  :   'bar',
     'host'      :   'foo.bar',
     'port'      :   '5432',
     'password'  :   'abc123'
